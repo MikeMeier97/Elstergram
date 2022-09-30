9e7a7f518766e7ffe206394a9e7dd1b5aab34c67
MikeMeier97 <m0.0m58@yahoo.de>
2022-09-30 21:22:52 +0200

    comment rendering completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+let comments = [
+  {
+    name: "MikeHowards",
+    commentText: "Sehr sehr schöner Ort",
+  },
+];
+
+function onload() {
+  renderComments();
+}
+
 function like() {
   document.getElementById("likeButton").src = "./img/heart-red.png";
+}
+
+function pushComment() {
+  console.log();
+}
+
+function renderComments() {
+  let commentField = document.getElementById("commentField");
+  commentField.innerHTML = ``;
+  for (let i = 0; i < comments.length; i++) {
+    let comment = comments[i];
+    commentField.innerHTML += `<p class="pl-2"><b>${comment["name"]} </b> ${comment["commentText"]}</p>`;
+  }
 }
